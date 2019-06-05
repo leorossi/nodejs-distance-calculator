2ba9239dc42d966f320773cd29e6b4d1a0eb62c4
Leonardo Rossi <leonardo.rossi@gmail.com>
2019-06-05 16:48:46 +0200

    re-enables all tests

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const path = require('path');
 require('dotenv').config({
   path: path.join(__dirname, '.env')
 });
-// require('./geocoder/test');
-// require('./coordinates_calculator/test');
-// require('./distance_matrix_calculator/test');
-// require('./locations_sanitizer/test');
 
+require('./geocoder/test');
+require('./coordinates_calculator/test');
+require('./distance_matrix_calculator/test');
+require('./locations_sanitizer/test');
 require('./closest_location_finder/test');
